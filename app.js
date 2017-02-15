8374ba8f986559379d506668b5909ad86044e75c
ItsMeEC <evan.ciavaglia3@gmail.com>
2017-02-15 13:26:35 -0500

    second js commit

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ function getAverageWordLength(tokens) {
 	var totalLength = tokens.join("").length;
 	return (totalLength / toekn.length).toFixed(2);
 }
+
+function countDistinctWords(tokens) {
+	var distinctWords = [];
+	for (var i = 0; i < tokens.length; i++) {
+		if (distinctWords.indexOf(tokens[i]) === -1) {
+			distinctWords.push(tokens[i]);
+		}
+	}
+	return distinctWords.length;
+}
